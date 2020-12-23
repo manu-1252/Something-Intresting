3e315a4f20e0ee3306edb0da0b1594cc21890dea
manu-1252 <43094769+manu-1252@users.noreply.github.com>
2020-12-23 21:48:24 +0530

    Update index.js

--- Wish-Her/message/js/index.js
+++ Wish-Her/message/js/index.js
@@ -1,6 +1,6 @@
 this.$dom = {};
 this.$dom.animatedText = document.createElement('div');
-this.text = "Happy Birthday!. You are so special in my life, not only for being my lovely sister, but also for being one of my best friends. On your special day I wish you only smiles and joy. May each minute of your life be filled with happiness and may this birthday be just perfect for you! My wish for you on your birthday is whatever you ask may you receive, whatever you seek may you find, whatever you wish may it be fulfilled on your birthday and always. \nOnce again Happy birthday Di...!";  // MESSAGE AREA , THIS CAN BE CHANGED FROM YOUR MESSAGE :)
+this.text = "Happy Birthday!. You are so special in my life, not only for being my best friend, but also for being one of my backbone in hard situations. On your special day I wish you only smiles and joy. May each minute of your life be filled with happiness and may this birthday be just perfect for you! My wish for you on your birthday is whatever you ask may you receive, whatever you seek may you find, whatever you wish may it be fulfilled on your birthday and always. \nOnce again Happy birthday Honey...!";  // MESSAGE AREA , THIS CAN BE CHANGED FROM YOUR MESSAGE :)
 this.$dom.container = document.getElementById('container');
 this.$dom.container.appendChild(this.$dom.animatedText);
 animateText();
